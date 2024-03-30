087d445321392bc271977fc49db8e5a3f4fb12fb
massimiliano <massimarcello@hotmail.it>
2024-03-30 03:00:03 +0100

    ok

--- components/Level/Level.tsx
+++ components/Level/Level.tsx
@@ -1,54 +1,38 @@
 
 
 
-import { useState,useEffect } from "react";
-// import calculateCardDistribution from '@/components/Calculator card distribuition/CalculatorCardDistribution';
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import theme from '@/app/theme_emotion';
-
 import Card from "@/components/SingleCard/SingleCard";
 import CardDistribution from "./CardDistribuitonProb";
 
-
-
 const TableCards = styled.div`
-    display: flex;
-    flex-wrap:wrap;
-`
-
-
-
-
-
-
-
-
-
-
+  display: flex;
+  flex-wrap:wrap;
+`;
 
 const LevelPage = ({ level }) => {
   const [visibleCards, setVisibleCards] = useState([]);
   const [credit, setCredit] = useState(100);
   const [remainingCards, setRemainingCards] = useState(20);
   const [betAmount, setBetAmount] = useState(0);
+  const [creditChangeMessage, setCreditChangeMessage] = useState(null);
 
   useEffect(() => {
-    // Calcola l'importo della scommessa dopo che gli stati sono stati aggiornati
     setBetAmount(calculateBetAmount());
   }, [credit, remainingCards]);
 
   const calculateBetAmount = () => {
-    return Math.round(credit / (2 * remainingCards));
+    return Math.round(credit / (0.5 * remainingCards));
   };
 
   const handleStartLevel = () => {
-    console.log("Starting level...");
     const newVisibleCards = CardDistribution({ level });
-    setVisibleCards(newVisibleCards.map((card) => ({ ...card, clicked: false })));
-    // Decrementa il numero di carte rimanenti
+    setVisibleCards(newVisibleCards.map(card => ({ ...card, clicked: false })));
     setRemainingCards(20);
-    // Riporta il credito al valore iniziale
     setCredit(100);
+    setCreditChangeMessage(null); // Resetta il messaggio di cambio credito quando avvii un nuovo livello
   };
 
   const handleCardClick = (index, suit) => {
@@ -57,19 +41,28 @@ const LevelPage = ({ level }) => {
     if (card.clicked) {
       return;
     }
-
+    
     const newVisibleCards = [...visibleCards];
     newVisibleCards[index].clicked = true;
 
     setRemainingCards(remainingCards - 1);
 
-    if (suit === "Positive") {
-      setCredit(credit + betAmount);
-    } else if (suit === "Negative") {
-      setCredit(credit - betAmount);
+    const currentBetAmount = calculateBetAmount();
+
+    if (suit === 'Positive') {
+      setCredit(credit + currentBetAmount);
+      setCreditChangeMessage(`Win +${currentBetAmount}`);
+    } else if (suit === 'Negative') {
+      setCredit(credit - currentBetAmount);
+      setCreditChangeMessage(`Loss -${currentBetAmount}`);
     }
 
     setVisibleCards(newVisibleCards);
+
+    // Nasconde il messaggio dopo un certo periodo di tempo
+    setTimeout(() => {
+      setCreditChangeMessage(null);
+    }, 2000);
   };
 
   return (
@@ -82,17 +75,16 @@ const LevelPage = ({ level }) => {
             key={index}
             imageUrl={card.clicked ? card.imageUrl : null}
             description={card.description}
-            suit={card.clicked ? card.suit : null}
-            onClick={() => handleCardClick(index, card.suit)}
+            suit={card.clicked ? card.suit : null} // Mostriamo il suit solo se la carta è stata cliccata
+            onClick={() => handleCardClick(index, card.suit)} // Passa l'indice e il suit alla funzione di gestione del clic
           />
         ))}
       </div>
-      <button onClick={handleStartLevel} disabled={remainingCards === 0}>
-        Start Level
-      </button>
+      <button onClick={handleStartLevel} disabled={remainingCards !== 0}>Start Level</button>
       <p>Credito rimanente: {credit}</p>
       <p>Carte rimanenti: {remainingCards}</p>
       <p>Bet Amount: {betAmount}</p>
+      {creditChangeMessage && <p>{creditChangeMessage}</p>}
     </div>
   );
 };
